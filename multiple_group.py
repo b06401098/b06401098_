c97be047e85e2645375cdcd42bec0711c7380bbe
b06401098 <36948444+b06401098@users.noreply.github.com>
2018-03-02 16:58:59 +0800

    中文化問題解決

--- multiple_group.py
+++ multiple_group.py
@@ -7,6 +7,8 @@ Created on Fri Mar  2 14:26:08 2018
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.font_manager import FontProperties
+font = FontProperties(fname=r"c:\windows\fonts\SimSun.ttc", size=14)
 
 df = pd.read_csv('Ntu_Orders.csv')
 
@@ -16,14 +18,18 @@ data = df.groupby(by=['LevelOneCategoryName','new_DateId'])['Quantity'].sum().re
 
 plot_list = []
 
-for i in set(data['LevelOneCategoryName'].values):
-    plot_list.append(data[data['LevelOneCategoryName']==i].groupby(by='new_DateId')['Quantity'].sum())
-
 fig, ((a,b),(c,d),(e,f)) = plt.subplots(3,2,figsize=(100,100))
 
-a.plot(plot_list[0])
-b.plot(plot_list[1])
-c.plot(plot_list[2])
-d.plot(plot_list[3])
-e.plot(plot_list[4])
-f.plot(plot_list[5])    
+layout_list = [a,b,c,d,e,f]
+
+index = 0
+for i in set(data['LevelOneCategoryName'].values):
+    plot_list.append(data[data['LevelOneCategoryName']==i].groupby(by='new_DateId')['Quantity'].sum())
+    layout_list[index].set_title(i,fontproperties=font)
+    print(i)
+    index = index+1
+index = 0
+for i in layout_list:
+    i.plot(plot_list[index])
+    index = index+1
+    
