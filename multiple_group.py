d4edea0e0b1bbd2b2d17cc0f9a06de6ff8731eaf
b06401098 <36948444+b06401098@users.noreply.github.com>
2018-03-02 15:59:25 +0800

    an multiple group graph example

---
+++ multiple_group.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  2 14:26:08 2018
+
+@author: user
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('Ntu_Orders.csv')
+
+df['new_DateId'] = pd.to_datetime(df['DateId'].astype(str),format='%Y%m%d')
+
+data = df.groupby(by=['LevelOneCategoryName','new_DateId'])['Quantity'].sum().reset_index()
+
+plot_list = []
+
+for i in set(data['LevelOneCategoryName'].values):
+    plot_list.append(data[data['LevelOneCategoryName']==i].groupby(by='new_DateId')['Quantity'].sum())
+
+fig, ((a,b),(c,d),(e,f)) = plt.subplots(3,2,figsize=(100,100))
+
+a.plot(plot_list[0])
+b.plot(plot_list[1])
+c.plot(plot_list[2])
+d.plot(plot_list[3])
+e.plot(plot_list[4])
+f.plot(plot_list[5])   
